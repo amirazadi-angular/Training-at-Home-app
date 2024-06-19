c2be14806b4279ceb9a96ac531b41b2de2482d51
amirazadi-angular <freedomeweb1400@gmail.com>
2024-06-19 18:39:01 +0330

    create Login and Signup

--- Training-at-home/src/app/auth/sign-up/sign-up.component.ts
+++ Training-at-home/src/app/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-
+  onSubmit(form : NgForm){
+    console.log(form);
+  }
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide);
+    event.stopPropagation();
+  }
 }
