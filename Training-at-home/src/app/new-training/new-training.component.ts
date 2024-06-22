dde2ce1a1d0dd3d8031641f1d8844ae6a96e45a5
amirazadi-angular <freedomeweb1400@gmail.com>
2024-06-22 21:58:17 +0330

    component training is styled

--- Training-at-home/src/app/new-training/new-training.component.ts
+++ Training-at-home/src/app/new-training/new-training.component.ts
@@ -3,14 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
-  styleUrl: './new-training.component.css'
+  styleUrl: './new-training.component.css',
 })
 export class NewTrainingComponent {
-  trainingArray : string[] = [
-    "Push-Ups",
-    "jogging",
-    "exercise bike",
-    "iron",
-    "sit-up"
-  ]
+  trainingArray: string[] = [
+    'Push-Ups',
+    'jogging',
+    'exercise bike',
+    'iron',
+    'sit-up',
+  ];
 }
